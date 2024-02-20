796a5f0dd39dd2319fb4b2b8b5eb351755e6d4c4
Juan Munoz <juan.pablo982206@gmail.com>
2024-02-20 09:07:27 -0500

    add custom exceptions handler

--- app/newsletter/application/services.py
+++ app/newsletter/application/services.py
@@ -4,32 +4,51 @@ from app.newsletter.domain.models import Campaign, Recipient, CampaignRecipient
 from app.newsletter.infrastructure.postgres.repository import EmailRepository
 from app.newsletter.utils.celery import send_email
 
+from app.shared import exceptions
+
 class EmailService:
     @staticmethod
     def send_campaign(email_campaign_id):
-        campaign = EmailRepository.get_campaign(email_campaign_id)
-        original_html_content = campaign.html_content
-        recipients: List[Recipient] = EmailRepository.get_campaign_recipients(email_campaign_id)
-        for recipient in recipients:
-            personalized_html_content = original_html_content.replace(
-                'UNSUBSCRIBE_LINK', 
-                f'http://localhost:5173/unsubscribe?user={recipient}'
-            )
-            send_email.delay(recipient, campaign.subject, personalized_html_content)
+        try:
+            campaign = EmailRepository.get_campaign(email_campaign_id)
+            if not campaign:
+                raise exceptions.CampaignNotFound
+            original_html_content = campaign.html_content
+            recipients: List[Recipient] = EmailRepository.get_campaign_recipients(email_campaign_id)
+            if not recipients:
+                raise exceptions.RecipientNotFound
+            for recipient in recipients:
+                personalized_html_content = original_html_content.replace(
+                    'UNSUBSCRIBE_LINK', 
+                    f'http://localhost:5173/unsubscribe?user={recipient}'
+                )
+                send_email.delay(recipient, campaign.subject, personalized_html_content)
+        except Exception as e:
+            raise e
 
     @staticmethod
     def create_campaign(name, description, category, subject, html_content):
-        campaign = Campaign(name=name, description=description, category=category, subject=subject, html_content=html_content)
-        EmailRepository.save_campaign(campaign)
-        return campaign
+        try:
+            campaign = Campaign(name=name, description=description, category=category, subject=subject, html_content=html_content)
+            campaign = EmailRepository.save_campaign(campaign)
+            if not campaign:
+                raise exceptions.ServiceNotAvaliableError
+            return campaign
+        except Exception as e:
+            raise e
 
     @staticmethod
     def create_recipients(emails):
-        recipients_tuple = EmailRepository.get_recipients_emails()
-        recipients = [recipient[0] for recipient in recipients_tuple]
-        new_emails = [email for email in emails if email not in recipients]
-        new_recipients = [Recipient(email=email) for email in new_emails]
-        EmailRepository.save_recipients(new_recipients)
+        try:
+            recipients_tuple = EmailRepository.get_recipients_emails()
+            if not recipients_tuple:
+                raise exceptions.ServiceNotAvaliableError
+            recipients = [recipient[0] for recipient in recipients_tuple]
+            new_emails = [email for email in emails if email not in recipients]
+            new_recipients = [Recipient(email=email) for email in new_emails]
+            EmailRepository.save_recipients(new_recipients)
+        except Exception as e:
+            raise e
 
     @staticmethod
     def add_recipient_to_campaign(email_campaign_id, recipient_emails):
@@ -37,11 +56,15 @@ class EmailService:
             CampaignRecipient(email_campaign_id=email_campaign_id, recipient_email=recipient_email)
             for recipient_email in recipient_emails
         ]
+        if not campaign_recipients:
+            raise exceptions.ServiceNotAvaliableError
         EmailRepository.save_campaign_recipients(campaign_recipients)
 
     @staticmethod
     def get_campaigns():
         campaigns = EmailRepository.get_campaigns()
+        if not campaigns:
+            raise exceptions.CampaignNotFound
         return campaigns
     
     @staticmethod
--- app/newsletter/routes/routes.py
+++ app/newsletter/routes/routes.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, request, jsonify
 
 from app.newsletter.application.services import EmailService
+from app.shared import exceptions
 
 newsletter_bp = Blueprint("newsletter", __name__)
 
@@ -12,39 +13,56 @@ def hello():
 
 @newsletter_bp.route("/create", methods=["POST"])
 def create_campaign():
-    data = request.json
-    name = data.get("name")
-    description = data.get("description")
-    category = data.get("category")
-    subject = data.get("subject")
-    html_content = data.get("html")
-    recipients_emails = data.get("emails")
-    campaign = EmailService.create_campaign(name, description, category, subject, html_content)
-    EmailService.create_recipients(recipients_emails)
-    EmailService.add_recipient_to_campaign(campaign.id, recipients_emails)
-    return jsonify({"message": "Campaign created successfully"})
+    try:
+        data = request.json
+        name = data.get("name")
+        description = data.get("description")
+        category = data.get("category")
+        subject = data.get("subject")
+        html_content = data.get("html")
+        recipients_emails = data.get("emails")
+        campaign = EmailService.create_campaign(name, description, category, subject, html_content)
+        EmailService.create_recipients(recipients_emails)
+        EmailService.add_recipient_to_campaign(campaign.id, recipients_emails)
+        return jsonify({"message": "Campaign created successfully"})
+    except exceptions.ServiceNotAvaliableError as e:
+        return jsonify({"message": str(e)}, 503)
+    except Exception as e:
+        return jsonify({"message": str(e)})
 
 
 @newsletter_bp.route("/campaigns", methods=["GET"])
 def get_campaigns():
-    campaigns = EmailService.get_campaigns()
-    serialized_campaigns = [
-        {
-            "id": campaign.id,
-            "name": campaign.name,
-            "description": campaign.description,
-            "category": campaign.category,
-            "subject": campaign.subject,
-            "html_content": campaign.html_content,
-        }
-        for campaign in campaigns
-    ]
-    return jsonify({"data": serialized_campaigns})
+    try:
+        campaigns = EmailService.get_campaigns()
+        serialized_campaigns = [
+            {
+                "id": campaign.id,
+                "name": campaign.name,
+                "description": campaign.description,
+                "category": campaign.category,
+                "subject": campaign.subject,
+                "html_content": campaign.html_content,
+            }
+            for campaign in campaigns
+        ]
+        return jsonify({"data": serialized_campaigns})
+    except exceptions.CampaignNotFound as e:
+        return jsonify({"message": str(e)}, 404)
+    except Exception as e:
+        return jsonify({"message": str(e)})
 
 @newsletter_bp.route("/send/<campaign_id>", methods=["POST"])
 def send_campaign(campaign_id):
-    EmailService.send_campaign(campaign_id)
-    return jsonify({"message": "Campaign sent successfully"})
+    try:
+        EmailService.send_campaign(campaign_id)
+        return jsonify({"message": "Campaign sent successfully"})
+    except exceptions.CampaignNotFound as e:
+        return jsonify({"message": str(e)}, 404)
+    except exceptions.RecipientNotFound as e:
+        return jsonify({"message": str(e)}, 404)
+    except Exception as e:
+        return jsonify({"message": str(e)})
 
 
 @newsletter_bp.route("/unsubscribe/<email>", methods=["POST"])
